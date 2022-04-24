c7fb1c78a5b6300f5d41550f0b0d13f22bdf5bf1
VadimZaets <79648897+VadimZaets@users.noreply.github.com>
2022-04-24 21:02:33 +0000

    Deploying to gh-pages from @ VadimZaets/privetinternet@2994a79c7e4935a05313134a4d76659e9eee9237 🚀

--- src.2c7d36b6.js
+++ src.2c7d36b6.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/privetinternet/src.f1ad6636.js.map
+//# sourceMappingURL=/privetinternet/src.2c7d36b6.js.map
